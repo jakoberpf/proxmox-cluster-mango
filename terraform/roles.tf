8dc92e36b2e5fe0499a79a24887e4a50068b801b
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2021-03-23 14:42:43 +0100

    add new roles for def and terraform

--- terraform/roles.tf
+++ terraform/roles.tf
@@ -1,3 +1,11 @@
 resource "openstack_identity_role_v3" "_member_" {
   name = "_member_"
 }
+
+resource "openstack_identity_role_v3" "developers" {
+  name = "developers"
+}
+
+resource "openstack_identity_role_v3" "terraform" {
+  name = "terraform"
+}
