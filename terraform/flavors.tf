b2978859048daa59042fb86707c2a2980f8b96bf
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2021-04-06 17:05:54 +0200

    make flavor public aviable

--- terraform/flavors.tf
+++ terraform/flavors.tf
@@ -1,34 +1,39 @@
 resource "openstack_compute_flavor_v2" "m1_tiny" {
-  name  = "m1.tiny"
-  ram   = "512"
-  vcpus = "1"
-  disk  = "1"
+  name      = "m1.tiny"
+  ram       = "512"
+  vcpus     = "1"
+  disk      = "2"
+  is_public = true
 }
 
 resource "openstack_compute_flavor_v2" "m1_small" {
-  name  = "m1.small"
-  ram   = "2048"
-  vcpus = "2"
-  disk  = "10"
+  name      = "m1.small"
+  ram       = "2048"
+  vcpus     = "2"
+  disk      = "10"
+  is_public = true
 }
 
 resource "openstack_compute_flavor_v2" "m1_medium" {
-  name  = "m1.medium"
-  ram   = "4096"
-  vcpus = "4"
-  disk  = "20"
+  name      = "m1.medium"
+  ram       = "4096"
+  vcpus     = "4"
+  disk      = "20"
+  is_public = true
 }
 
 resource "openstack_compute_flavor_v2" "m1_large" {
-  name  = "m1.large"
-  ram   = "8192"
-  vcpus = "4"
-  disk  = "40"
+  name      = "m1.large"
+  ram       = "8192"
+  vcpus     = "6"
+  disk      = "40"
+  is_public = true
 }
 
 resource "openstack_compute_flavor_v2" "m1_xlarge" {
-  name  = "m1.xlarge"
-  ram   = "16384"
-  vcpus = "4"
-  disk  = "80"
+  name      = "m1.xlarge"
+  ram       = "16384"
+  vcpus     = "8"
+  disk      = "80"
+  is_public = true
 }
