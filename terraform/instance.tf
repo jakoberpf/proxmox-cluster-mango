80a435254290e8f2ce4eac38d7cccc4629688619
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2021-04-06 12:44:26 +0200

    add another test instance

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -13,3 +13,19 @@ resource "openstack_compute_instance_v2" "basic" {
     name = "test"
   }
 }
+
+resource "openstack_compute_instance_v2" "ubuntu" {
+  name            = "ubuntu"
+  image_name      = "Ubuntu20.04"
+  flavor_name     = "m1.large"
+  key_pair        = "jakoberpf"
+  security_groups = ["default"]
+
+  metadata = {
+    this = "that"
+  }
+
+  network {
+    name = "test"
+  }
+}
