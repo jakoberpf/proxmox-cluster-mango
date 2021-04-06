82f51206ee187241d970e60996545002bc1f1ceb
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2021-04-06 10:41:04 +0200

    add secgroups

---
+++ terraform/secgroups.tf
@@ -0,0 +1,23 @@
+resource "openstack_compute_secgroup_v2" "icmp" {
+  name        = "icmp"
+  description = "Allow icmp ingress"
+
+  rule {
+    from_port   = -1
+    to_port     = -1
+    ip_protocol = "icmp"
+    cidr        = "0.0.0.0/0"
+  }
+}
+
+resource "openstack_compute_secgroup_v2" "ssh" {
+  name        = "ssh"
+  description = "Allow ssh ingress"
+
+  rule {
+    from_port   = 22
+    to_port     = 22
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+}
