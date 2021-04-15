02df6fbcacca005b7e05791452c66a16e6493638
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2021-04-15 23:41:24 +0200

    update flavours

--- terraform/flavors.tf
+++ terraform/flavors.tf
@@ -1,6 +1,6 @@
 resource "openstack_compute_flavor_v2" "m1_tiny" {
   name      = "m1.tiny"
-  ram       = "512"
+  ram       = "1024"
   vcpus     = "1"
   disk      = "2"
   is_public = true
@@ -17,7 +17,7 @@ resource "openstack_compute_flavor_v2" "m1_small" {
 resource "openstack_compute_flavor_v2" "m1_medium" {
   name      = "m1.medium"
   ram       = "4096"
-  vcpus     = "4"
+  vcpus     = "2"
   disk      = "20"
   is_public = true
 }
@@ -25,7 +25,7 @@ resource "openstack_compute_flavor_v2" "m1_medium" {
 resource "openstack_compute_flavor_v2" "m1_large" {
   name      = "m1.large"
   ram       = "8192"
-  vcpus     = "6"
+  vcpus     = "4"
   disk      = "40"
   is_public = true
 }
@@ -33,7 +33,7 @@ resource "openstack_compute_flavor_v2" "m1_large" {
 resource "openstack_compute_flavor_v2" "m1_xlarge" {
   name      = "m1.xlarge"
   ram       = "16384"
-  vcpus     = "8"
+  vcpus     = "6"
   disk      = "80"
   is_public = true
 }
