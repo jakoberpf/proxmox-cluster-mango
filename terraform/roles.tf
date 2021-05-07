b47333e5e415716f7548df0b5c161490c689ae74
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2021-05-07 23:47:03 +0200

    remove unsued code

--- terraform/roles.tf
+++ terraform/roles.tf
@@ -4,7 +4,3 @@ resource "openstack_identity_role_v3" "admin" {
 resource "openstack_identity_role_v3" "developers" {
   name = "developers"
 }
-
-# resource "openstack_identity_role_v3" "terraform" {
-#   name = "terraform"
-# }
