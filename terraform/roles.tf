6cf4f75ed5ee58e6b39876ef7b10f3ab91033711
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2021-05-08 00:11:00 +0200

    prvent admin role from deletion

--- terraform/roles.tf
+++ terraform/roles.tf
@@ -1,5 +1,8 @@
 resource "openstack_identity_role_v3" "admin" {
   name = "admin"
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 resource "openstack_identity_role_v3" "developers" {
   name = "developers"
