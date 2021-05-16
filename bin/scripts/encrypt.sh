5dcac41340865ff75151e70a8e378e327c04bc6d
Jakob <Erpf>
2021-05-16 16:12:42 +0200

    update envcryption script

--- bin/scripts/encrypt.sh
+++ bin/scripts/encrypt.sh
@@ -13,7 +13,13 @@ secrets+=("terraform/admin/files/logins")
 secrets+=("terraform/services/.envrc")
 secrets+=("terraform/services/terraform.tfstate")
 
-# TODO tar octavia and encrypt
+cd ansible/artifacts/octavia/
+tar -cf client_ca.tar.xz client_ca
+tar -cf server_ca.tar.xz server_ca
+cd $GIT_ROOT
+
+secrets+=("ansible/artifacts/octavia/client_ca.tar.xz")
+secrets+=("ansible/artifacts/octavia/server_ca.tar.xz")
 
 echo ""
 echo " #######################"
@@ -25,3 +31,6 @@ for secret in "${secrets[@]}";
 do
     gpg --encrypt --recipient jakoberpf $GIT_ROOT/$secret
 done
+
+rm ansible/artifacts/octavia/client_ca.tar.xz
+rm ansible/artifacts/octavia/server_ca.tar.xz
