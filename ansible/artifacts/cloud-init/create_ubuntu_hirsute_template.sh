493fede7ca85015fd5806b9d1a1e5c644cc6c894
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2021-09-19 09:06:40 +0200

    update template creation scripts

--- ansible/artifacts/cloud-init/create_ubuntu_hirsute_template.sh
+++ ansible/artifacts/cloud-init/create_ubuntu_hirsute_template.sh
@@ -2,44 +2,44 @@
 cd /var/lib/vz/template/iso
 
 # Source the image
-wget https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img
+wget https://cloud-images.ubuntu.com/hirsute/current/hirsute-server-cloudimg-amd64.img
 
 apt-get install libguestfs-tools
-virt-customize -a focal-server-cloudimg-amd64.img --install qemu-guest-agent
+virt-customize -a hirsute-server-cloudimg-amd64.img --install qemu-guest-agent
 
 # Create the instance
-qm create 9000 -name debian-cloudinit -memory 1024 -net0 virtio,bridge=vmbr0 -cores 1 -sockets 1
+qm create 9002 -name ubuntu-hirsute-cloudinit -memory 1024 -net0 virtio,bridge=vmbr0 -cores 1 -sockets 1
 
 # Import the OpenStack disk image to Proxmox storage
-qm importdisk 9000 focal-server-cloudimg-amd64.img local-lvm
+qm importdisk 9002 hirsute-server-cloudimg-amd64.img local-lvm
 
 # Attach the disk to the virtual machine
-qm set 9000 -scsihw virtio-scsi-pci -virtio0 local-lvm:vm-9000-disk-0
+qm set 9002 -scsihw virtio-scsi-pci -virtio0 local-lvm:vm-9002-disk-0
 
 # Add a serial output
-qm set 9000 -serial0 socket
+qm set 9002 -serial0 socket
 
 # Set the bootdisk to the imported Openstack disk
-qm set 9000 -boot c -bootdisk virtio0
+qm set 9002 -boot c -bootdisk virtio0
 
 # Enable the Qemu agent
-qm set 9000 -agent 1
+qm set 9002 -agent 1
 
 # Allow hotplugging of network, USB and disks
-qm set 9000 -hotplug disk,network,usb
+qm set 9002 -hotplug disk,network,usb
 
 # Add a single vCPU (for now)
-qm set 9000 -vcpus 1
+qm set 9002 -vcpus 1
 
 # Add a video output
-qm set 9000 -vga qxl
+qm set 9002 -vga qxl
 
 # Set a second hard drive, using the inbuilt cloudinit drive
-qm set 9000 -ide2 local-lvm:cloudinit
+qm set 9002 -ide2 local-lvm:cloudinit
 
 # Resize the primary boot disk (otherwise it will be around 2G by default)
 # This step adds another 8G of disk space, but change this as you need to
-qm resize 9000 virtio0 +8G
+qm resize 9002 virtio0 +8G
 
 # Convert the VM to the template
-qm template 9000
+qm template 9002
