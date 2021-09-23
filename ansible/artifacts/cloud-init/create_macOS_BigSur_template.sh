9e639824ffb010ab764ee8a4a9addb6b24e90afd
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2021-09-23 23:38:02 +0200

    add scripts

---
+++ ansible/artifacts/cloud-init/create_macOS_BigSur_template.sh
@@ -0,0 +1,2 @@
+# https://pve.proxmox.com/pve-docs/qm.1.html
+# https://manjaro.site/install-macos-big-sur-11-on-proxmox-6-3/
---
+++ bin/scripts/setup-ansible.sh
@@ -0,0 +1,4 @@
+# Get roles from galaxy
+ansible-galaxy install systemli.letsencrypt
+
+# Get roles from repos
