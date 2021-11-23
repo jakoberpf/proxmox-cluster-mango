9b556ff21c8e45b1ab455e0b0eb3a5de8aee053b
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2021-11-23 17:17:15 +0100

    update terraform structure

--- terraform/zerotier.tf
+++ terraform/zerotier.tf
@@ -4,18 +4,18 @@
 resource "zerotier_identity" "proxmox" {}
 
 resource "zerotier_member" "proxmox" {
-  name           = "proxmox"
+  name           = "Proxmox-Development-0"
   member_id      = zerotier_identity.proxmox.id
   network_id     = var.zerotier_network_id
   ip_assignments = [var.zerotier_proxmox_ip]
 }
 
 resource "local_file" "private_key" {
-    content  = zerotier_identity.proxmox.private_key
-    filename = "../../ansible/artifacts/zerotier/identity.secret"
+  content  = zerotier_identity.proxmox.private_key
+  filename = "../ansible/artifacts/zerotier/identity.secret"
 }
 
 resource "local_file" "public_key" {
-    content  = zerotier_identity.proxmox.public_key
-    filename = "../../ansible/artifacts/zerotier/identity.public"
+  content  = zerotier_identity.proxmox.public_key
+  filename = "../ansible/artifacts/zerotier/identity.public"
 }
