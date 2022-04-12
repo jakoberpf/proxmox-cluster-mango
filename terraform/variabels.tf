62b9ae81854f0e149d07f6656635d371607fc84e
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-04-12 11:07:59 +0200

    update variable handling

--- terraform/variabels.tf
+++ terraform/variabels.tf
@@ -1,3 +1,7 @@
+variable "authorized_keys" {
+  type        = string
+  description = ""
+}
 variable "cloudflare_email" {
   type        = string
   description = ""
@@ -23,10 +27,31 @@ variable "cloudflare_account_id" {
   description = ""
 }
 
+variable "proxmox_terraform_user" {
+  type        = string
+  description = ""
+}
+
+variable "proxmox_terraform_password" {
+  type        = string
+  description = ""
+}
+
+variable "proxmox_terraform_host" {
+  type        = string
+  description = ""
+}
+
 variable "zerotier_central_token" {
   type        = string
   description = "The API token of the zerotier API"
 }
+
+variable "zerotier_network_id_development" {
+  type        = string
+  description = "The zerotier network id of the development network"
+}
+
 variable "zerotier_network_id" {
   type        = string
   description = "The ID of the zerotier network"
