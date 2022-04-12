790a02d7423a6a5d189fd409b5718eb6024a454a
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-04-12 09:26:50 +0200

    update zerotier

--- terraform/zerotier.tf
+++ terraform/zerotier.tf
@@ -1,13 +1,12 @@
-# https://registry.terraform.io/providers/zerotier/zerotier/latest/docs/resources/member
-# https://registry.terraform.io/providers/zerotier/zerotier/latest/docs/resources/identity
-
 resource "zerotier_identity" "proxmox" {}
 
 resource "zerotier_member" "proxmox" {
-  name           = "Proxmox-Development-0"
-  member_id      = zerotier_identity.proxmox.id
-  network_id     = var.zerotier_network_id
-  ip_assignments = [var.zerotier_proxmox_ip]
+  name       = "proxmox-development"
+  member_id  = zerotier_identity.proxmox.id
+  network_id = var.zerotier_network_id
+  ip_assignments = [
+    var.zerotier_proxmox_ip
+  ]
 }
 
 resource "local_file" "private_key" {
